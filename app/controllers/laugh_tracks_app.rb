b2093c9b27901971c25aea226ed5691778459868
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-01-26 11:04:08 -0700

    files weren't added to commit intended to turn in. all files present

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,2 +1,14 @@
 class LaughTracksApp < Sinatra::Base
+
+  get '/comedians' do
+    @comedians = Comedian.all
+    @specials = Special.all
+    erb :"/index"
+  end
+
+  get '/comedians/[:age]' do
+    @comedians = Comedian.all
+    @comedian = Comedian.find_by(params[:age])
+  end
+
 end
